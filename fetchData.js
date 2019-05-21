2ebecbffb4b1317daa37732af11afc50103139e6
hozfia <48630338+hozfia@users.noreply.github.com>
2019-05-22 01:00:17 +0200

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -1,7 +1,5 @@
-//this script is used to fetch data from the Database.
-
+//this script uses ajax object to append php's echo to the message box whome id is #messagebox
 function loadMsg() {
-    
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
